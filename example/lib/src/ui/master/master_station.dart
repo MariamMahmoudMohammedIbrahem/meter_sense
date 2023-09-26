e486ceb762efa312853a2d687430bea1c61e80d4
MariamMahmoudMohammedIbrahem <mariam.mahmoud2811@gmail.com>
2023-09-26 14:22:48 +0200

    commit

--- example/lib/src/ui/master/master_station.dart
+++ example/lib/src/ui/master/master_station.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:flutter_reactive_ble_example/src/ble/functions.dart';
 import 'package:flutter_reactive_ble_example/src/ui/SQFLITE/sqldb.dart';
 import 'package:functional_data/functional_data.dart';
 import 'package:provider/provider.dart';
@@ -10,11 +11,16 @@ import 'package:provider/provider.dart';
 import '../../ble/ble_device_connector.dart';
 import '../../ble/ble_device_interactor.dart';
 import '../../ble/constants.dart';
+import '../SQFLITE/dataPage.dart';
 import '../device_detail/device_interaction_tab.dart';
 
 List<int> dataList = [89, 0, 0, 0, 153, 0, 0, 0, 0, 0, 0, 0, 0, 67, 148, 0, 0, 0, 1, 0];
 
 Uint8List byteData = Uint8List.fromList(dataList);
+num electricTarrif = 0;
+num electricBalance = 0;
+num waterTarrif = 0;
+num waterBalance = 0;
 
 class MasterInteractionTab extends StatelessWidget {
   const MasterInteractionTab({
@@ -112,99 +118,228 @@ class _MasterStation extends StatefulWidget {
 class _MasterStationState extends State<_MasterStation> {
   @override
   StreamSubscription<List<int>>? subscribeStream;
+  Future<void> writeCharacteristicWithoutResponse() async {
+    int chunkSize = 20;
+    for (int i = 0; i < myList.length; i += chunkSize) {
+      int end = i + chunkSize;
+      if (end > myList.length) {
+        end = myList.length;
+      }
+      List<int> chunk = myList.sublist(i, end);
+      print("Sending chunk: $chunk");
+      await widget.writeWithoutResponse(widget.characteristic, chunk);
+    }
+  }
+  Future<void> subscribeCharacteristic() async {
+    // var newEventData =<int>[];
+    if (kDebugMode) {
+      print("subscribe in");
+    }
+    subscribeStream =
+        widget.subscribeToCharacteristic(widget.characteristic).listen((event) {
+          //electric tarrif
+          if(event.first == 0xA3){
+            electricTarrif = convertToInt(event, 0, 1);
+            print("electric tarrif:$electricTarrif");
+          }
+          if(event.first == 0xA4){
+            electricBalance = convertToInt(event, 0, 1);
+            print("electric balance:$electricBalance");
+          }
+          if(event.first == 0xA5){
+            waterTarrif = convertToInt(event, 0, 1);
+            print("water tarrif:$waterTarrif");
+          }
+          if(event.first == 0xA6){
+            waterBalance = convertToInt(event, 0, 1);
+            print("water balance:$waterBalance");
+          }
+        });
+  }
   void initState() {
-    // if(!widget.viewModel.deviceConnected){
-    //   widget.viewModel.connect();
-    // }
+    timer = Timer.periodic(interval, (Timer t) async {
+      if(!widget.viewModel.deviceConnected){
+        widget.viewModel.connect();
+      }
+      // else if(widget.viewModel.deviceConnected){
+      //   await subscribeCharacteristic();
+      //   await widget.readCharacteristic(widget.characteristic);
+      // }
+      else{
+        t.cancel();
+      }
+    });
     final myInstance = SqlDb();
     myInstance.getList();
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    Future<void> subscribeCharacteristic() async {
-      // var newEventData =<int>[];
-      if (kDebugMode) {
-        print("subscribe in");
-      }
-      subscribeStream =
-          widget.subscribeToCharacteristic(widget.characteristic).listen((event) {
-
-              testing = event;
-              print("testing$testing");
-          });
-    }
+    final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsetsDirectional.only(start: 16.0),
-            child: Text(
-              "Connection: ${widget.viewModel.connectionStatus}",
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-          ElevatedButton(
-              onPressed: ()async {
-                try {
-                  await widget.writeWithoutResponse(widget.characteristic, myList);
-                  print("byteData:$myList");
-                  // Data has been sent successfully.
-                } catch (e) {
-                  // Handle any errors that might occur during the write operation.
-                  print("Error sending data: $e");
-                }
-                if (kDebugMode) {
-                  print("Error doesn't exist");
-                }
-              },
-              child: const Text("get data", style: TextStyle(color: Colors.black),),
-          ),
-          ElevatedButton(
-            onPressed: ()async {
-              if(!widget.viewModel.deviceConnected){
-                widget.viewModel.connect();
-              }
-              else{
-                await subscribeCharacteristic();
-                await widget.readCharacteristic(widget.characteristic);
-              }
-              print("responseread:$testing");
-            },
-            child: const Text("button 2", style: TextStyle(color: Colors.black),),
-          ),
-          ElevatedButton(
-            onPressed: ()async {},
-            child: const Text("button 3", style: TextStyle(color: Colors.black),),
-          ),
-          SizedBox(
-            height: height*.5,
-            child: Column(
+      body: RefreshIndicator(
+        onRefresh: ()=> Future.delayed(
+            const Duration(seconds: 1),(){
+          setState(() {
+            if(!widget.viewModel.deviceConnected){
+              widget.viewModel.connect();
+            }
+            else if(widget.viewModel.deviceConnected){
+              subscribeCharacteristic();
+              widget.readCharacteristic(widget.characteristic);
+            }
+          });
+        }),
+        child: ListView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: count,
-                    itemBuilder: (BuildContext context, int index) {
-                      // Check if the item is a padding item (empty string)
-                      if (count == 0) {
-                        return const SizedBox(
-                          child: Text("There is no data"),
-                        );
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 16.0),
+                  child: Text(
+                    "Connection: ${widget.viewModel.connectionStatus}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: ()async {
+                      if(!widget.viewModel.deviceConnected){
+                        widget.viewModel.connect();
                       }
                       else{
-                        return const SizedBox(
-                          child: Text("There is data"),
-                        );
+                        await writeCharacteristicWithoutResponse();
+                        print("byteData:$myList");
                       }
                     },
+                    child: const Text("get data", style: TextStyle(color: Colors.black),),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:width*.07,vertical: 10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(width:width*.07),
+                          const Text(
+                            'ele Tarrif: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            electricTarrif.toString(),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(width:width*.07),
+                          const Text(
+                            'ele Balance: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            electricBalance.toString(),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(width:width*.07),
+                          const Text(
+                            'water Tarrif: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            waterTarrif.toString(),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(width:width*.07),
+                          const Text(
+                            'water Balance: ',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            waterBalance.toString(),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: ()async {
+                    await subscribeCharacteristic();
+                    await widget.readCharacteristic(widget.characteristic);
+                  },
+                  child: const Text("update", style: TextStyle(color: Colors.black),),
+                ),
+                SizedBox(
+                  height: height*.5,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: count,
+                          itemBuilder: (BuildContext context, int index) {
+                            // Check if the item is a padding item (empty string)
+                            if (count == 0) {
+                              return const SizedBox(
+                                child: Text("There is no data"),
+                              );
+                            }
+                            else{
+                              return const SizedBox(
+                                child: Text("There is data"),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
